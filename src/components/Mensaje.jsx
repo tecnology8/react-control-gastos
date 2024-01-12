368ce779f15da9178cbc8e97372b05807301a162
Tecnology8 <ruddycruceta@hotmail.com>
2024-01-12 12:13:16 -0400

    First Coommit

---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Mensaje = ({children , tipo}) =>{
+    return (
+
+        <div className={`alerta ${tipo}`}>
+            {children}
+        </div>
+    )
+}
+
+
+export default Mensaje
